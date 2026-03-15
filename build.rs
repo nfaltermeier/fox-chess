ae67df1aeb6de5ca3a169090eb6836bb9c73d9d8
nfaltermeier <nefalt2@gmail.com>
2026-03-15 16:25:18 -0500

    Convert lmp from floating point to fixed point
    I wasn't expecting this to be a gainer, neat
    
    Results of engine1 vs engine2 (8+0.08, NULL, 16MB, Pohl.epd):
    Elo: 5.55 +/- 4.36, nElo: 8.10 +/- 6.36
    LOS: 99.37 %, DrawRatio: 40.36 %, PairsRatio: 1.08
    Games: 11452, Wins: 3333, Losses: 3150, Draws: 4969, Points: 5817.5 (50.80 %)
    Ptnml(0-2): [305, 1335, 2311, 1422, 353], WL/DD Ratio: 1.09
    LLR: 2.94 (101.6%) (-2.25, 2.89) [0.00, 10.00]
    
    Bench Nodes: 22520615 NPS: 8275979 Time: 2.7212026s

--- build.rs
+++ build.rs
@@ -6,17 +6,34 @@ fn main() {
     build_info_build::build_script();
 
     let out_dir = env::var_os("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("king_attack_unit_values.rs");
 
-    let king_attack_unit_values = generate_king_attack_unit_values();
+    {
+        let dest_path = Path::new(&out_dir).join("king_attack_unit_values.rs");
 
-    let king_attack_unit_values_code = quote! {
-        pub static KING_ATTACK_UNIT_VALUES: [i16; 100] = [#(#king_attack_unit_values),*];
-    };
+        let king_attack_unit_values = generate_king_attack_unit_values();
 
-    let parsed_code = syn::parse_file(king_attack_unit_values_code.to_string().as_str()).unwrap();
+        let king_attack_unit_values_code = quote! {
+            pub static KING_ATTACK_UNIT_VALUES: [i16; 100] = [#(#king_attack_unit_values),*];
+        };
 
-    fs::write(&dest_path, prettyplease::unparse(&parsed_code)).unwrap();
+        let parsed_code = syn::parse_file(king_attack_unit_values_code.to_string().as_str()).unwrap();
+
+        fs::write(&dest_path, prettyplease::unparse(&parsed_code)).unwrap();
+    }
+
+    {
+        let dest_path = Path::new(&out_dir).join("ln_fixedpoint_128_values.rs");
+
+        let ln_fixedpoint_128_values = generate_ln_fixedpoint_128_values();
+
+        let ln_fixedpoint_128_values_code = quote! {
+            pub static LN_FIXEDPOINT_128_VALUES: [i16; 64] = [#(#ln_fixedpoint_128_values),*];
+        };
+
+        let parsed_code = syn::parse_file(ln_fixedpoint_128_values_code.to_string().as_str()).unwrap();
+
+        fs::write(&dest_path, prettyplease::unparse(&parsed_code)).unwrap();
+    }
 }
 
 fn generate_king_attack_unit_values() -> Box<[i16; 100]> {
@@ -25,6 +42,16 @@ fn generate_king_attack_unit_values() -> Box<[i16; 100]> {
     for (i, v) in result.iter_mut().enumerate() {
         let power = (35.0 - i as f32) / 10.0;
         *v = ((500.0 * 1.46) / (1.0 + 4.0_f32.powf(power))).round() as i16
+    }
+
+    result
+}
+
+fn generate_ln_fixedpoint_128_values() -> Box<[i16; 64]> {
+    let mut result = Box::new([0; 64]);
+
+    for (i, v) in result.iter_mut().enumerate() {
+        *v = ((i as f32).ln() * 128.0).round() as i16;
     }
 
     result
